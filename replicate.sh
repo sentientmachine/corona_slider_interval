8776a22b76769154b4087d5c8a7a5ec8fd04feb3
Eric <eric.leschinski@hotmail.com>
2020-05-08 20:34:08 -0400

    new data

--- replicate.sh
+++ replicate.sh
@@ -4,7 +4,7 @@
 #you'll need webspace and some nonzero experience in front end web development
 #mount point has to be set manually
 
-cp -r /home/el/bin/corona_slider_interval/javascripts /mnt/machinesentience/public_html
+#cp -r /home/el/bin/corona_slider_interval/javascripts /mnt/machinesentience/public_html
 cp /home/el/bin/corona_slider_interval/covid19.html /mnt/machinesentience/public_html/covid19.html
 
 #cp /home/el/bin/corona_slider_interval/main.html /mnt/machinesentience/public_html/main.html
